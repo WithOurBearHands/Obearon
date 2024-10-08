163fb3554676c6db54d5136e4d7203cdaaa6f97b
Deko <github.com.quadrant163@passinbox.com>
2024-10-08 13:35:51 +0200

    uncomment headless option

--- bearification/browser.py
+++ bearification/browser.py
@@ -31,7 +31,7 @@ class Browser:
         firefox_options.set_preference("general.useragent.override", user_agent)
         firefox_options.add_argument("--width=1920")
         firefox_options.add_argument("--height=1080")
-        # firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--headless")
         self.browser = webdriver.Firefox(options=firefox_options)
 
     def close(self):
