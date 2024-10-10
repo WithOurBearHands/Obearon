40a95f082f9c45746144de4bff2713583880b930
Deko <github.com.quadrant163@passinbox.com>
2024-10-10 11:55:26 +0200

    load dotenv optionally to allow docker env file

--- bearification/database/engine.py
+++ bearification/database/engine.py
@@ -2,15 +2,12 @@
 Database engine related module.
 """
 
-from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from bearification.database.models import GuildRole
 from bearification.database.models import User
-
-load_dotenv()
 
 async_engine = create_async_engine("sqlite+aiosqlite:///data/bearification.db")
 async_session = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
--- main.py
+++ main.py
@@ -3,12 +3,17 @@ The main file to control Bearification.
 """
 
 import asyncio
+import os
 
 from bearification.database import engine
 from bearification.discord_bot import close
 from bearification.discord_bot import start_discord_bot
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    if not os.environ.get("DISCORD_TOKEN"):
+        load_dotenv()
+
     event_loop = asyncio.get_event_loop_policy().new_event_loop()
     try:
         event_loop.run_until_complete(engine.create_tables())
